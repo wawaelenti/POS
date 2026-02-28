87c10cfa47d3a63c8d51c42970a5c35b831df691
wawa elent <irawantielent@gmail.com>
2026-02-28 18:29:06 +0700

    Praktikum5

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
