375d11665d22800de5c4ba1acaeea16594a0a0c4
wawa elent <irawantielent@gmail.com>
2026-02-28 19:00:15 +0700

    Praktikum6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function profile($id, $name)
+    public function index()
     {
-        return view('user.profile', compact('id','name'));
+        //tambah data user dengan Eloquent Model
+        $data = [
+            'nama' => 'Pelangan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data); //update data user
+        
+        //coba akses model UserModel
+        $user = UserModel::all(); //ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
     }
 }
 
