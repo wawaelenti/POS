1c9dfca564c1a8d66743761ac42c425c42d1d052
wawa elent <irawantielent@gmail.com>
2026-03-02 20:18:22 +0700

    Praktikum 2.1 js 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,17 +10,9 @@ class UserController extends Controller
 {
     public function index()
     {
-        //tambah data user dengan Eloquent Model
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
-        
-        //coba akses model UserModel
-        $user = UserModel::all(); //ambil semua data dari tabel m_user
+        $user = UserModel::findOr(20, ['username', 'nama'], function() {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Data User</title>
 </head>
+
 <body>
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
@@ -12,14 +14,13 @@
             <th>Nama</th>
             <th>ID Level Pengguna</th>
         </tr>
-        @foreach ($data as $d)
         <tr>
-            <td>{{ $d->user_id }}</td>
-            <td>{{ $d->username }}</td>
-            <td>{{ $d->nama }}</td>
-            <td>{{ $d->level_id }}</td>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
         </tr>
-        @endforeach
     </table>
 </body>
+
 </html>
