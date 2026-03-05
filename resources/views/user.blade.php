3164b18ae252ff76b1a4f37e3bd346b981e9268c
wawa elent <irawantielent@gmail.com>
2026-03-06 00:03:01 +0700

    Praktikum 2.4 js 4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -9,10 +9,16 @@
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <td>Jumlah Pengguna</td>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
         </tr>
         <tr>
-            <td>{{ $data }}</td>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
         </tr>
     </table>
 </body>
