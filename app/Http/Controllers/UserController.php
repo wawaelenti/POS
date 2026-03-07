14286cd0ebbd0f963329506fb87d712c729b21f9
wawa elent <irawantielent@gmail.com>
2026-03-07 23:41:58 +0700

    Praktikum 2.5 js 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,19 +10,21 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33'
-            ],
-            [
-                'nama' => 'Manager Tiga Tiga',
+        $user = UserModel::create([
+                'username' => 'manager11',
+                'nama' => 'Manager11',
                 'password' => Hash::make('12345'),
-                'level_id' => 2
-            ]
-        );
+                'level_id' => 2,
+            ]);
 
-        $user->save();
+            $user->username ='manager 12';
 
-        return view('user', ['data' => $user]);
+            $user->save();
+
+            $user->wasChanged(); //true
+            $user->wasChanged('username'); //true
+            $user->wasChanged(['username', 'level_id']); //true
+            $user->isClean('nama'); //false
+            dd($user->wasChanged(['nama', 'username'])); //true
     }
 }
