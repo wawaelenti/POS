2ed4e1fc5f0825d36bee5fafb7c34b0aa5769674
wawa elent <irawantielent@gmail.com>
2026-03-08 09:40:39 +0700

    Praktikum 2.6 js 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,21 +10,53 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::create([
-                'username' => 'manager11',
-                'nama' => 'Manager11',
-                'password' => Hash::make('12345'),
-                'level_id' => 2,
-            ]);
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
 
-            $user->username ='manager 12';
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
 
-            $user->save();
+    public function tambah_simpan(Request $request)
+    {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make('$request->password'),
+            'level_id' => $request->level_id 
+        ]);
 
-            $user->wasChanged(); //true
-            $user->wasChanged('username'); //true
-            $user->wasChanged(['username', 'level_id']); //true
-            $user->isClean('nama'); //false
-            dd($user->wasChanged(['nama', 'username'])); //true
+        return redirect('/user');
+    }
+
+    public function ubah($id)
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_Simpan($id, Request $request)
+    {
+        $user =UserModel::find($id);
+
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make($request->password);
+        $user->level_id = $request->level_id;
+
+        $user->save();
+        return redirect('/user');
+    }
+
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user ->delete();
+
+        return redirect('/user');
     }
 }
+
+
